68ae23389ad8406809087ec910bce4ffee6abe08
Denis Kolovorotnyi <denis.kolovorotnyi@septhora.com>
2023-06-18 09:51:29 +0200

    fixed run in docker

---
+++ sql/create-db.sql
@@ -0,0 +1,4 @@
+create database "rangiffler-auth" with owner postgres;
+create database "rangiffler-geo" with owner postgres;
+create database "rangiffler-photo" with owner postgres;
+create database "rangiffler-userdata" with owner postgres;
