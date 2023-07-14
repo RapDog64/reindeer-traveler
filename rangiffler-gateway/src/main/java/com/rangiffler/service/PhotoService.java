d0d74c492f304f3bf24e615babba0f8b7ffa8346
Denis Kolovorotnyi <denis.kolovorotnyi@septhora.com>
2023-07-14 13:10:39 +0200

    [fix]: Fixed bug with friends photos

--- rangiffler-gateway/src/main/java/com/rangiffler/service/PhotoService.java
+++ rangiffler-gateway/src/main/java/com/rangiffler/service/PhotoService.java
@@ -1,10 +1,12 @@
 package com.rangiffler.service;
 
 
-import com.rangiffler.model.*;
+import com.rangiffler.model.CountryJson;
+import com.rangiffler.model.FriendStatus;
+import com.rangiffler.model.PhotoJson;
+import com.rangiffler.model.PhotoServiceJson;
 import com.rangiffler.service.configuration.PhotoServiceClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -42,17 +44,12 @@ public class PhotoService {
     }
 
     public List<PhotoJson> getAllFriendsPhotos(String username) {
-        List<PhotoJson> friendsPhoto = new ArrayList<>();
-        List<UserJson> friends = userService.receivePeopleAround(username)
+        return userService.receivePeopleAround(username)
                 .stream()
                 .filter(userJson -> userJson.getFriendStatus() == FriendStatus.FRIEND)
-                .toList();
-
-        for (UserJson userJson : friends) {
-            friendsPhoto.addAll(getAllUserPhotos(userJson.getUsername()));
-        }
-
-        return friendsPhoto;
+                .map(userJson -> getAllUserPhotos(userJson.getUsername()))
+                .findFirst()
+                .orElseGet(ArrayList::new);
     }
 
     public void deletePhoto(UUID photoId) {
