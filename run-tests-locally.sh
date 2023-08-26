ed3176bb164d34255c6947ecd184db5bf26c9b87
Denis <denis152324@gmail.com>
2023-08-26 12:53:36 +0200

    [refactor]: Changed bash run test file

--- run-tests-locally.sh
+++ run-tests-locally.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo '### Running the tests ###'
-bash ./gradlew :rangiffler-e-2-e-tests:clean test -Denv=docker
+if [ "$1" = "docker" ]; then
+  bash ./gradlew :rangiffler-e-2-e-tests:clean test -Denv=docker
+else
+  bash ./gradlew :rangiffler-e-2-e-tests:clean test
+fi
 echo '### Preparing the report ###'
 bash ./gradlew :rangiffler-e-2-e-tests:allureServe
